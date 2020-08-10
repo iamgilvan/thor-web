265c413be3dd821142e8d76b29f7d4215697e89f
Gilvan Almeida <gilvan.praxedes@outlook.com>
2020-08-09 22:42:20 -0300

    Fixing erro utils

--- utils/error_utils.py
+++ utils/error_utils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-class Error_Utils(self):
+class Error_Utils:
     __errors = dict([
         # Error genéricos
         (400, 'BAD REQUEST'),
@@ -8,9 +8,8 @@ class Error_Utils(self):
     ])
 
 
-    @staticmethod
     def get_message(self, id):
-        if id in ErrorUtils.__errors:
-            return ErrorUtils.__errors.get(id)
+        if id in self.__errors:
+            return self.__errors.get(id)
         else:
             return "UNEXPECTED ERROR. PLEASE CONTACT US AT {gilvan.praxedes@outlook.com}"

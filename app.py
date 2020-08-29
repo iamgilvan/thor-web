0bb5e7936db64131a7d9c7fb54538ae6249c07e0
Gilvan Almeida <gilvan.praxedes@outlook.com>
2020-08-29 16:21:06 -0300

    Move secret key correct file

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template
-import secrets
-import os
-
-secret = secrets.token_urlsafe(32)
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-app.secret_key = os.urandom(24)
+#app.secret_key = secret
 
 from views.mcdm import *
 
